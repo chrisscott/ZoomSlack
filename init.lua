85c6f764c6f4a2930a17ff4dd30ea8df144dabab
Chris Scott <chris.scott@auth0.com>
2019-09-13 15:01:53 -0400

    initial commit of simple detection

--- init.lua
+++ init.lua
@@ -1,6 +1,22 @@
 --- === ZoomSlack ===
 ---
---- Set your Slack status when in a Zoom meeting. Clear it when you"re done.
+--- Set your Slack status when in a Zoom meeting. Clear it when you're done.
+---
+--- You'll need to use a [legacy token] (https://api.slack.com/custom-integrations/legacy-tokens)
+--- and pass it in to the configuration along with any other configuration options. e.g.:
+---
+--- local zoom = hs.spoons.use(
+---   "ZoomSlack", 
+---   {
+---     config = { 
+---       slackToken = "[YOUR SLACK TOKEN]",
+---       statusEmoji = ":zoom:"
+---     }
+---   }
+--- )
+--- if zoom then
+---   spoon.ZoomSlack:start()
+--- end
 ---
 --- https://github.com/chrisscott
 
@@ -38,20 +54,24 @@ obj.statusEmoji = ":spiral_calendar_pad:"
 --- 1H
 obj.expiration = "N"
 
-
 --- ZoomSlack.logger
 --- Variable
 --- Logger object used within the Spoon. Can be accessed to set the default log level for the messages coming from the Spoon.
 obj.logger = hs.logger.new("ZoomSlack")
 
-obj.sawMeetingWindow = false
-obj.sawShareWindow = false
+--- ZoomSlack.dryRun
+--- Variable
+--- When set to true, don't send messages to the Slack API and don't send notifications. Defaults to false.
+obj.dryRun = false
 
 --- ZoomSlack:start()
 --- Method
 --- Start ZoomSlack
 ---
 --- Parameters:
+---  * None
+---
+--- Returns:
 ---  * None
 function obj:start()
   assert(obj.slackToken, "Slack token must be provided")
@@ -60,61 +80,56 @@ function obj:start()
     elseif obj.expiration == "30M" then obj.status_expiration_seconds = 1800
     elseif obj.expiration == "45M" then obj.status_expiration_seconds = 2700
     elseif obj.expiration == "1H" then obj.status_expiration_seconds = 3600
+    else obj.status_expiration_seconds = 0
   end
 
-  local wf = hs.window.filter.new(false):setAppFilter("zoom.us")
+  obj.logger.df("Status expiration set to '%s' which is '%d' in seconds", obj.expiration, obj.status_expiration_seconds)
 
-  -- for when a meeting is started from the app's main window vs. browser
-	wf:subscribe("windowTitleChanged", function(window, name)
-		obj.logger.d("Window title changed. Setting sawMeetingWindow to true")
-		obj.logger.df("sawShareWindow is %s", obj.sawShareWindow)
-		obj.sawMeetingWindow = true
-		if not obj.sawShareWindow then
-			obj.setSlackStatus()
-		end
-  end)
-
-  -- for when a meeting is started from the browser
-	wf:subscribe("windowCreated", function(window, name)
-		obj.logger.d("Window created")
-		print(string.format("title %s", window:title()))
-		print(string.format("role %s", window:role()))
-		print(string.format("subrole %s", window:subrole()))
-		if window:subrole() == "AXSystemDialog" and window:title() == "" then
-			obj.logger.d("Looks like a screen share. setting sawShareWindow to true")
-			obj.sawShareWindow = true
-		end
-
-		if string.find(window:title(), "Zoom Meeting ID") then
-			obj.logger.d("Found Zoom Meeting window, setting status, setting sawMeetingWindow to true")
-			obj.sawMeetingWindow = true
-      obj.setSlackStatus()
+  hs.application.watcher.new(function(name, event, app)
+    if name == 'zoom.us' then
+      if event == hs.application.watcher.launched then
+        obj.setSlackStatus()
+      elseif event == hs.application.watcher.terminated then
+        obj.clearSlackStatus()
+      end
     end
-  end)
+  end):start()
+end
 
-  wf:subscribe("windowDestroyed", function(window, name)
-    local app = window:application()
-		local meetingWindow = app:findWindow("Zoom Meeting ID")
-		obj.logger.d("Window destroyed")
-		obj.logger.df("meetingWindow is %s", meetingWindow)
+function obj:setSlackStatus()
+  local status_expiration = obj.status_expiration_seconds + os.time(os.date("*t"))
 
-		if (meetingWindow) then
-			obj.logger.d("Setting sawMeetingWindow to true")
-      obj.sawMeetingWindow = true
-		end
-		
-		obj.logger.df("sawMeetingWindow is %s, sawShareWindow is %s", obj.sawMeetingWindow, obj.sawShareWindow)
+  obj.logger.i("Setting Slack status")
+  obj.logger.df("Setting Slack status '%s' with emoji '%s' and an expiration in %d seconds (unixtime of %d)",
+    obj.statusText,
+    obj.statusEmoji,
+    obj.status_expiration_seconds,
+    status_expiration
+  )
 
-    -- make sure the meeting was ended and the modal window wasn't canceled instead
-    if (obj.sawMeetingWindow and not meetingWindow) or (obj.sawMeetingWindow and obj.sawShareWindow) then
-      obj.sawMeetingWindow = false
-      obj.clearSlackStatus()
-    end
-    end)
+  obj:sendStatus(obj.statusText, obj.statusEmoji, status_expiration)
+  if not obj.dryRun then
+    hs.notify.show("", "Zoom Started", "Slack status set")
+  end
+end
+
+function obj:clearSlackStatus()
+  obj.logger.i("Clearing Slack status")
+
+  obj:sendStatus("", "", 0)
+  if not obj.dryRun then
+    hs.notify.show("", "Zoom Terminated", "Slack status cleared")
+  end
 end
 
 function obj:sendStatus(text, emoji, expiration)
+  if obj.dryRun then
+    obj.logger.d("Dry run enabled. POSTing to Slack API disabled.")
+    return
+  end
+
   obj.logger.d("POSTing to Slack API")
+
   local jsonreq = {
     profile = {
       status_text = text,
@@ -122,6 +137,7 @@ function obj:sendStatus(text, emoji, expiration)
       status_expiration = expiration
     }
   }
+
   hs.http.asyncPost(
     "https://slack.com/api/users.profile.set",
     hs.json.encode(jsonreq),
@@ -129,35 +145,24 @@ function obj:sendStatus(text, emoji, expiration)
       ["Authorization"] = "Bearer " .. obj.slackToken, 
       ["Content-type"] = "application/json; charset=utf-8"
     },
-		function(http_code, body)
-			local res = hs.json.decode(body)
-			if http_code == 200 then
-				if not res.ok then
-					hs.notify.new({title = "Setting Slack Status Failed!", informativeText = res.error}):send()
-					obj.logger.df("Slack API response error: %s", res.error)
-					return false
-				end
-			else
-					hs.notify.new({title = "Setting Slack Status Failed!", informativeText = res.error}):send()
-					obj.logger.df("Slack API response error: %s", res.error)
-					return false
-			end
+    function(http_code, body)
+      local res = hs.json.decode(body)
 
+      obj.logger.df("Slack API response body: \n%s", body)
+      
+      if http_code == 200 then
+        if not res.ok then
+          hs.notify.new({title = "Setting Slack Status Failed!", informativeText = res.error}):send()
+          obj.logger.df("Slack API response error: %s", res.error)
+          return false
+        end
+      else
+          hs.notify.new({title = "Setting Slack Status Failed!", informativeText = res.error}):send()
+          obj.logger.df("Slack API response error: %s", res.error)
+          return false
+      end
     end
   )
-end
-
-function obj:setSlackStatus()
-	local status_expiration = obj.status_expiration_seconds * os.time(os.date("!*t"))
-	obj.logger.d("Setting Slack status '%s' with emoji '%s' and an expiration in %s seconds", obj.statusText, obj.statusEmoji, obj.status_expiration_seconds)
-	obj:sendStatus(obj.statusText, obj.statusEmoji, status_expiration)
-	hs.notify.show("", "Zoom Started", "Slack status set")
-end
-
-function obj:clearSlackStatus()
-	obj.logger.d("Clearing Slack status")
-	obj:sendStatus("", "", 0)
-	hs.notify.show("", "Zoom Stopped", "Slack status cleared")
 end
 
 return obj
